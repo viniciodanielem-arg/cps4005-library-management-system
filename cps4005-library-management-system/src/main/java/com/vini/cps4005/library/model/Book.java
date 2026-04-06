32887c76c87dfeaa5a47f1dc3491d84cc465d9b2
Vinicio Daniele <viniciodanielem@gmail.com>
2026-04-06 12:40:52 +0100

    Initial Book.java Class

--- cps4005-library-management-system/src/main/java/com/vini/cps4005/library/model/Book.java
+++ cps4005-library-management-system/src/main/java/com/vini/cps4005/library/model/Book.java
@@ -9,9 +9,49 @@ package com.vini.cps4005.library.model;
  * @author Daniele
  */
 public class Book {
-    private int id;
+    private int bookId;
     private String title;
     private String author;
     private String category; 
-    private boolean availability;
+    private String availabilityStatus;
+    
+    public Book() { }
+    
+    public Book (int bookId, String title, String author, String category, String availabilityStatus) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.availabilityStatus = availabilityStatus;
+    }
+    
+        public Book (String title, String author, String category, String availabilityStatus) {
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.availabilityStatus = availabilityStatus;
+    }
+    
+    //Getters:
+    public int getBookId() { return bookId; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getCategory() { return category; }
+    public String getAvailabilityStatus() { return availabilityStatus; }
+    
+    //Setters:
+    public void setBookId(int bookId) { this.bookId = bookId; }
+    public void setTitle(String title) { this.title = title; }
+    public void setAuthor(String author) { this.author = author; }
+    public void setCategory(String category) { this.category = category; }
+    public void setAvailabilityStatus(String availabilityStatus) { this.availabilityStatus = availabilityStatus; }
+    
+    @Override
+    public String toString() {
+        return "Book ID: " + bookId +
+               ", Title: " + title +
+               ", Author: " + author +
+               ", Category: " + category +
+               ", Status: " + availabilityStatus;
+    }
 }
