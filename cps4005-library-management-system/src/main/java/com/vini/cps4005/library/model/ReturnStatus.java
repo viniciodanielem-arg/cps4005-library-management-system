f1fa4204dcf9a559c4b35c283e38422a970e1190
Vinicio Daniele <viniciodanielem@gmail.com>
2026-04-10 16:24:41 +0100

    return status enum

---
+++ cps4005-library-management-system/src/main/java/com/vini/cps4005/library/model/ReturnStatus.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.vini.cps4005.library.model;
+
+/**
+ *
+ * @author Daniele
+ */
+public enum ReturnStatus {
+    BORROWED,
+    RETURNED,
+    OVERDUE
+}
