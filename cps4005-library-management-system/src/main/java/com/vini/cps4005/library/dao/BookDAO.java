f0c86279729afe936534cae58ea77f493871e193
Vinicio Daniele <viniciodanielem@gmail.com>
2026-04-10 09:21:54 +0100

    Book DAO written

--- cps4005-library-management-system/src/main/java/com/vini/cps4005/library/dao/BookDAO.java
+++ cps4005-library-management-system/src/main/java/com/vini/cps4005/library/dao/BookDAO.java
@@ -8,6 +8,175 @@ package com.vini.cps4005.library.dao;
  *
  * @author Daniele
  */
+import com.vini.cps4005.library.model.Book;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import com.vini.cps4005.library.util.DatabaseConnection;
+
 public class BookDAO {
+    /*
+    - Build the table in the sqlite database,
+    - Make methods for adding, deleting, and updating (e.g. title, category, availability status)
+    - Make methods for displaying/ searching by title, author, or ID.
+
+    */
+    public void createTable() {
+        String sql = """
+                CREATE TABLE IF NOT EXISTS books (
+                    book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    title TEXT NOT NULL,
+                    author TEXT NOT NULL,
+                    category TEXT NOT NULL,
+                    availability_status TEXT NOT NULL
+                )
+                     """;
+        try (Connection conn = DatabaseConnection.connect();
+             Statement stmt = conn.createStatement()) {
+            
+            stmt.execute(sql);
+            System.out.println("Books table ready.");
+            
+        } catch (SQLException e) {
+            System.out.println("Error creating books table: " + e.getMessage());
+        }
+    }
+    
+    public boolean addBook(Book book) {
+        String sql = "INSERT INTO books (title, author, category, availability_status) VALUES (?, ?, ?, ?)";
+        
+        try (Connection conn = DatabaseConnection.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            
+            pstmt.setString(1, book.getTitle());
+            pstmt.setString(1, book.getAuthor());
+            pstmt.setString(1, book.getCategory());
+            pstmt.setString(1, book.getAvailabilityStatus());
+            
+            int rowsAffected = pstmt.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            System.out.println("Error adding book: " + e.getMessage());
+            return false;
+        }
+    }
+    
+    public List<Book> getAllBooks() {
+        List<Book> books = new ArrayList<>();
+        String sql = "SELECT * FROM books";
+        
+        try (Connection conn = DatabaseConnection.connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            
+            while (rs.next()) {
+                Book book = new Book(
+                        rs.getInt("book_id"),
+                        rs.getString("title"),
+                        rs.getString("author"),
+                        rs.getString("category"),
+                        rs.getString("availability_status")
+                );
+                books.add(book);
+            }
+            
+        } catch (SQLException e) {
+            System.out.println("Error retrieving books: " + e.getMessage());
+        }
+        
+        return books;
+    }
+    
+    public Book getBookById(int bookId) {
+        String sql = "SELECT * FROM books WHERE book_id = ?";
+        
+        try (Connection conn = DatabaseConnection.connect();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                 
+            pstmt.setInt(1, bookId);
+            ResultSet rs = pstmt.executeQuery();
+            
+            if (rs.next()) {
+                return new Book(
+                        rs.getInt("book_id"),
+                        rs.getString("title"),
+                        rs.getString("author"),
+                        rs.getString("category"),
+                        rs.getString("availability_status")
+                );
+            }
+              
+        } catch (SQLException e) {
+            System.out.println("Error finding book: " + e.getMessage());
+        }
+        
+        return null;
+    }
+    
+    public List<Book> searchBooksByTitle(String title) {
+        List<Book> books = new ArrayList<>();
+        String sql = "SELECT * FROM books WHERE title LIKE ?";
+        
+        try (Connection conn = DatabaseConnection.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            
+            pstmt.setString(1, "%" + title + "%");
+            ResultSet rs = pstmt.executeQuery();
+            
+            while (rs.next()) {
+                books.add(new Book(
+                        rs.getInt("book_id"),
+                        rs.getString("title"),
+                        rs.getString("author"),
+                        rs.getString("category"),
+                        rs.getString("availability_status")
+                ));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error searching for books" + e.getMessage());
+        }
+        
+        return books;
+    }
+    
+    public boolean updateBook(Book book) {
+        String sql = "UPDATE books SET title = ?, author = ?, category = ?, , availability_status = ? WHERE book_id = ?";
+        
+        try (Connection conn = DatabaseConnection.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            
+            pstmt.setString(1, book.getTitle());
+            pstmt.setString(2, book.getAuthor());
+            pstmt.setString(3, book.getCategory());
+            pstmt.setString(4, book.getAvailabilityStatus());
+            pstmt.setInt(5, book.getBookId());
+            
+            int rowsAffected = pstmt.executeUpdate();
+            return rowsAffected > 0;
+            
+        } catch (SQLException e) {
+            System.out.println("Error updating book: " + e.getMessage());
+            return false;
+        }
+    
+    }
+    
+    public boolean deleteBook(int bookId) {
+        String sql = "DELETE FROM books WHERE book_id = ?";
+        
+        try (Connection conn = DatabaseConnection.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            
+            pstmt.setInt(1, bookId);
+            
+            int rowsAffected = pstmt.executeUpdate();
+            return rowsAffected > 0;
+            
+        } catch (SQLException e) {
+            System.out.println("Error deleting book: " + e.getMessage());
+            return false;
+        }
+    }
+    
     
 }
