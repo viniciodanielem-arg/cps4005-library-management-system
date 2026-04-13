b59bacea2d7d56410605d9ade8ce1c6417a7b79f
Vinicio Daniele <viniciodanielem@gmail.com>
2026-04-13 16:04:10 +0100

    MemberService written

--- cps4005-library-management-system/src/main/java/com/vini/cps4005/library/service/BookService.java
+++ cps4005-library-management-system/src/main/java/com/vini/cps4005/library/service/BookService.java
@@ -10,130 +10,81 @@ package com.vini.cps4005.library.service;
  */
 import com.vini.cps4005.library.dao.BookDAO;
 import com.vini.cps4005.library.model.Book;
-import java.util.ArrayList;
-import java.util.Scanner;
+
+import java.util.List;
 
 public class BookService {
     
-    Scanner sc = new Scanner(System.in);
-    BookDAO bookDAO = new BookDAO();
+    private final BookDAO bookDAO;
     
-    public void addBook() {
-        // ASK USER FOR BOOK DETAILS: title, author category
-        // book_id is authomatic and set the availability_status to RETURNED
-        // then add the book to the database (use validation beforehand)
+    public BookService() {
+        this.bookDAO = new BookDAO();
+    }
+    
+    // Create book object
+    public boolean addBook(String title, String author, String category) {
         
-        System.out.println("Please enter a book title: ");
-        String titleInput  = sc.nextLine();
-
-        System.out.println("Please enter an author: ");
-        String authorInput  = sc.nextLine();
-
-        System.out.println("Please enter a category: ");
-        String categoryInput  = sc.nextLine();
+        if (title == null || title.isBlank()) return false;
+        if (author == null || author.isBlank()) return false;
+        if (category == null || category.isBlank()) return false;
         
-        Book addingBook = new Book(titleInput, authorInput, categoryInput, "Available");
+        Book book = new Book(title, author, category, "Available");
         
-        if (bookDAO.addBook(addingBook)) {
-            System.out.println("Successfully added book");
-        }
-        
+        return bookDAO.addBook(book);
         
     }
     
-    public void displayAllBooks() {
-        ArrayList<Book> allBooks = (ArrayList<Book>) bookDAO.getAllBooks();
-        
-        for (Book book : allBooks) {
-            book.toString();
-            System.out.print("\n");
-        }
+    // Read all books
+    public List<Book> getAllBooks() {
+        return bookDAO.getAllBooks();
     }
     
-    public void searchForBook() {
-        int choice;
-        
-        do {
-            System.out.println("Choose search method: ");
-            System.out.println("1. Search by Id");
-            System.out.println("2. Search by title");
-            System.out.println("3. Search by author");
-            System.out.println("4. Go Back");
-            
-            choice = Integer.parseInt(sc.nextLine());
-            
-            switch (choice) {
-                case 1:
-                    System.out.println("Enter an Id: ");
-                    int id = sc.nextInt();
-                    Book book1 = bookDAO.getBookById(id);
-                    System.out.println(book1.toString());
-                    break;
-                    
-                case 2:
-                    System.out.println("Enter a title: ");
-                    String title = sc.nextLine();
-                    ArrayList<Book> booksFound1 = (ArrayList<Book>) bookDAO.searchBooksByTitle(title);
-                    for (Book book : booksFound1) {
-                        System.out.println(book.toString());
-                        System.out.print("\n");
-                    }
-                    break;
-                    
-                case 3:
-                    System.out.println("Enter an Author: ");
-                    String author = sc.nextLine();
-                    ArrayList<Book> booksFound2 = (ArrayList<Book>) bookDAO.searchBooksByAuthor(author);
-                    for (Book book : booksFound2) {
-                        System.out.println(book.toString());
-                        System.out.print("\n");
-                    }
-                    break;
-                    
-                case 4:
-                    System.out.println("Returning...");
-                    break;
-                    
-                default:
-                    System.out.println("Invalid choice");
-            }
-        } while (choice != 4);
+    
+    // Read by Id
+    public Book getBookById(int id) {
+        return bookDAO.getBookById(id);
     }
     
-    public void updateBook() {
-        System.out.println("Please enter a book ID number to proceed: ");
-        int bookId = sc.nextInt();
-        Book book = bookDAO.getBookById(bookId);
+    
+    
+    // Search for a book
+    public List<Book> searchByTitle(String title) {
+        return bookDAO.searchBooksByTitle(title);
+    }
+    
+    public List<Book> searchByAuthor(String author) {
+        return bookDAO.searchBooksByAuthor(author);
+    }    
+    
+    
+    
+    public boolean updateBook(int id, String title, String author, String category) {
         
-        System.out.println("\n Current book details: \n");
-        System.out.println(book.toString() + "\n");
+        Book existing = bookDAO.getBookById(id);
         
-        System.out.println("Please enter title update");
-        String titleInput  = sc.nextLine();
-
-        System.out.println("Please enter author update");
-        String authorInput  = sc.nextLine();
-
-        System.out.println("Please enter category update");
-        String categoryInput  = sc.nextLine();
+        if (existing == null) return false;
         
-        Book updatedBook = new Book(bookId, titleInput, authorInput, categoryInput, "Available");
+        if(title == null || title.isBlank()) title = existing.getTitle();
+        if(author == null || author.isBlank()) author = existing.getAuthor();
+        if(category == null || category.isBlank()) category = existing.getCategory();        
         
-        if (bookDAO.updateBook(updatedBook)) {
-            System.out.println("Successfully updated book " + bookId + " info");
-        }
+        Book updatedBook = new Book(
+                id,
+                title,
+                author,
+                category,
+                existing.getAvailabilityStatus()
+        );
         
+        return bookDAO.updateBook(updatedBook);
         
     }
     
-    public void deleteBook() {
-        System.out.println("Please enter a book ID number to proceed: ");
-        int bookId = sc.nextInt();
-        
-        if (bookDAO.deleteBook(bookId)) {
-            System.out.println("Successfully deleted book " + bookId + " info");
-        }
-        
-              
+    
+    
+    public boolean deleteBook(int id) {
+        return bookDAO.deleteBook(id);
     }
+    
+    
 }
--- cps4005-library-management-system/src/main/java/com/vini/cps4005/library/service/MemberService.java
+++ cps4005-library-management-system/src/main/java/com/vini/cps4005/library/service/MemberService.java
@@ -6,6 +6,9 @@ package com.vini.cps4005.library.service;
 
 import com.vini.cps4005.library.model.MembershipType;
 import com.vini.cps4005.library.dao.MemberDAO;
+import com.vini.cps4005.library.model.Member;
+
+import java.util.List;
 
 /**
  *
@@ -13,11 +16,74 @@ import com.vini.cps4005.library.dao.MemberDAO;
  */
 public class MemberService {
         
-    MemberDAO memberDAO = new MemberDAO();
+    private final MemberDAO memberDAO;
     
+    public MemberService() {
+        this.memberDAO = new MemberDAO(); 
+    }
+    
+    // Check if member has reached their limit: 
     public boolean canBorrow(int memberId, MembershipType type) {
         int currentBorrowed = memberDAO.getActiveBorrowCount(memberId);
         
         return currentBorrowed < type.getBorrowingLimit();
+    }
+    
+    // Create Member Object
+    
+    public boolean addMember(String name, String email, MembershipType membershipType) {
+        
+        if (name == null || name.isEmpty()) return false;
+        if (email == null || email.isEmpty()) return false;
+        if (membershipType == null || membershipType.toString().isEmpty()) return false;
+        
+        Member member = new Member(name, email, membershipType);
+        
+        return memberDAO.addMember(member);
+    }
+    
+    // Read all members from db
+    
+    public List<Member> getAllMembers() {
+        return memberDAO.getAllMembers();
+    }
+    
+    // Read by Id
+    
+    public Member getMemberById(int id) {
+        return memberDAO.getMemberById(id);
+    }
+    
+    // Read by name
+    
+    public List<Member> getMemberByName(String name) {
+        return memberDAO.getMemberByName(name);
+    }
+    
+    // Update 
+    
+    public boolean updateMember(int id, String name, String email, MembershipType membershipType) {
+        Member existing = memberDAO.getMemberById(id);
+        if (existing == null) return false;
+        
+        if(name == null || name.isBlank()) name = existing.getName();
+        if(email == null || email.isBlank()) email = existing.getEmail();
+        if(membershipType == null || membershipType.toString().isBlank()) membershipType = existing.getMembershipType();
+    
+        Member updatedMember = new Member(
+                id,
+                name,
+                email,
+                membershipType
+        );
+        
+        return memberDAO.updateMember(updatedMember);
+        
+    }
+    
+    // Delete
+    
+    public boolean deleteMember(int id) {
+        return memberDAO.deleteMember(id);
     }
 }
